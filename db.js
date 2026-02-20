984daa353a22e16425808f899ef10aa8c1d08345
Humancare11 <webdev@humancareworldwide.com>
2026-02-20 17:47:23 +0530

    db env update

--- db.js
+++ db.js
@@ -1,7 +1,15 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+// await mongoose.connect("mongodb+srv://satishdalvi2210_db_user:Satish123@cluster0.9lai1xz.mongodb.net/?appName=Cluster0");
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-await mongoose.connect("mongodb+srv://satishdalvi2210_db_user:Satish123@cluster0.9lai1xz.mongodb.net/?appName=Cluster0");
+  await mongoose.connect(process.env.MONGO_URL);
 };
 
 module.exports = connectDB;
